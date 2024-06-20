ee360378b2b3d6c092dbc50c9df7b4edc71d04d2
Anuj <anujtiwari2822@gmail.com>
2024-06-20 10:33:40 +0530

    configure redux and add css

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,14 +1,41 @@
-
+import { useState,useEffect } from 'react';
+import {useDispatch} from 'react-redux';
+import authService from './appwrite/auth';
 import './App.css'
+import {login,logout} from './store/authSlice';
+import { Header,Footer } from './components';
+
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  //console.log(import.meta.env.VITE_APPWRITE_URL);
+  const[loading,setLoading] = useState(true);
+   const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1> Hello world from Anuj</h1>
-    </>
-  )
+   useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {cd
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-red-400'>
+    <div className='w-full block'>
+      <Header />
+      <main>
+      TODO:   
+      </main>
+      <Footer />
+    </div>
+  </div>
+) : null
+  
 }
 
 export default App
