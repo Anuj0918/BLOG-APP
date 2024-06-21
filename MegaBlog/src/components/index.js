c8aaeb00237b1bab8faa54552beac4fdc40c6a4f
Anuj <anujtiwari2822@gmail.com>
2024-06-21 10:21:47 +0530

    components added

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,5 +1,10 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/Logoutbtn";
+import Container from "./container/container";
 
 
-export {Header,Footer};
+export {Header,Footer,
+    Logo,LogoutBtn,Container
+    };
